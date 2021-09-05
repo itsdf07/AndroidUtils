a3eb81b859ee3091b2c43d31576f3af0ac0bc8c6
itsdf07 <923255742@qq.com>
2021-09-05 11:38:51 +0800

    Initial commit project

---
+++ app/src/main/java/top/itaso/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package top.itaso.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
